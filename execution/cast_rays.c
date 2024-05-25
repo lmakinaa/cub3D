fd6b3110536d41fdfd73f6e64dba693ab55e4d27
Lmakinaa <jaija.ismail@gmail.com>
2024-05-25 16:49:35 +0100

    pseudo code of intersections

--- execution/cast_rays.c
+++ execution/cast_rays.c
@@ -6,11 +6,45 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 19:17:40 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/25 01:17:17 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/25 16:38:06 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "cub3d.h"
+
+	// first horizental intersec:
+	/*
+		y = floor(p_y / TILE_SIZE) * TILE_SIZE
+		x = p_x + (p_y - y / tan(angle))
+	*/
+	
+	// horizental intersec steps
+	/*
+		step_y = opposite = TILE_SIZE
+		step_x = opposite / tan(angle)
+	*/
+void	h_intersec(t_cub *cub, int first)
+{
+	if (first)
+	{
+		
+	}
+}
+
+void	v_intersec(t_cub *cub, int first)
+{
+	// first horizental intersec:
+	/*
+		x = ????(p_x / TILE_SIZE) * TILE_SIZE
+		y = p_y - ((x - p_x) * tan(angle))
+	*/
+	
+	// horizental intersec steps
+	/*
+		step_x = adjacent = TILE_SIZE
+		step_y = opposite * tan(angle)
+	*/
+}
 
 void cast_rays(t_cub *cub, int x, int y)
 {
