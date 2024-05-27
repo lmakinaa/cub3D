3b9f59a9ff1130753b5b3087b5eb2134f8e247e3
Lmakinaa <jaija.ismail@gmail.com>
2024-05-27 21:21:51 +0100

    hmm

--- execution/update_vars.c
+++ execution/update_vars.c
@@ -6,55 +6,51 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 01:23:44 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/27 18:46:26 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/27 21:21:37 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static	void	p_movemenet(t_cub *cub, int *new_x, int *new_y)
+void	p_movement(t_cub *mlx, double move_x, double move_y)
 {
-	int	step;
+	int		new_x;
+	int		new_y;
 
-	if (cub->p->l_r == 1)
+	new_y = roundf(mlx->p->y_pixel + move_y);
+	new_x = roundf(mlx->p->x_pixel + move_x);
+	if (mlx->data->map2d[new_y / TILE_SIZE][new_x / TILE_SIZE] != '1')
 	{
-		step = PLAYER_SPEED;
-		(*new_x) = roundf(cub->p->mini_y_pixel - sin(cub->p->angle) * step);
-		(*new_y) = roundf(cub->p->mini_x_pixel + cos(cub->p->angle) * step);
-	}
-	else if (cub->p->l_r == -1)
-	{
-		step = PLAYER_SPEED;
-		(*new_x) = roundf(cub->p->mini_y_pixel + sin(cub->p->angle) * step);
-		(*new_y) = roundf(cub->p->mini_x_pixel - cos(cub->p->angle) * step);
-	}
-	else if (cub->p->u_d != 0)
-	{
-		step = cub->p->u_d * PLAYER_SPEED;
-		(*new_x) = roundf(cub->p->mini_x_pixel + cos(cub->p->angle) * step);
-		(*new_y) = roundf(cub->p->mini_y_pixel + sin(cub->p->angle) * step);
+		mlx->p->x_pixel = new_x;
+		mlx->p->y_pixel = new_y;
+		mlx->p->mini_x_pixel = new_x;
+		mlx->p->mini_y_pixel = new_y;
 	}
 }
 
 void	update_vars(t_cub *cub)
 {
-	int new_x;
-	int new_y;
-	int	tmp_x;
-	int	tmp_y;
+	double	tmp_x;
+	double	tmp_y;
+	double	step;
 
 	cub->p->angle = map_angle(cub->p->angle + cub->p->rot * ROTATION_SPEED);
-	p_movemenet(cub, &new_x, &new_y);
-	tmp_x = new_x / MINI_TILE_SIZE;
-	tmp_y = new_y / MINI_TILE_SIZE;
-	if (cub->data->map2d[tmp_y][tmp_x] != '1')
+	step = PLAYER_SPEED;
+	if (cub->p->l_r == 1)
 	{
-		tmp_x = (new_x + MINI_TILE_SIZE / 2) / MINI_TILE_SIZE;
-		tmp_y = (new_y + MINI_TILE_SIZE / 2) / MINI_TILE_SIZE;
-		if (cub->data->map2d[tmp_y][tmp_x] != '1')
-		{
-			(1) && (cub->p->mini_x_pixel = new_x, cub->p->mini_y_pixel = new_y);
-			(1) && (cub->p->x_pixel = new_x, cub->p->y_pixel = new_y);
-		}
+		tmp_x = -1 * sin(cub->p->angle) * step;
+		tmp_y = cos(cub->p->angle) * step;
 	}
+	else if (cub->p->l_r == -1)
+	{
+		tmp_x = sin(cub->p->angle) * step;
+		tmp_y = -1 * cos(cub->p->angle) * step;
+	}
+	else if (cub->p->u_d != 0)
+	{
+		step *= cub->p->u_d;
+		tmp_x = cos(cub->p->angle) * step;
+		tmp_y = sin(cub->p->angle) * step;
+	}
+	p_movement(cub, tmp_x, tmp_y);
 }
