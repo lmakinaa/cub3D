1883a3114b88d415217cfcd4c9b5d64cd6962cb2
Lmakinaa <jaija.ismail@gmail.com>
2024-05-27 18:46:31 +0100

    fixed raycast

--- execution/update_vars.c
+++ execution/update_vars.c
@@ -6,7 +6,7 @@
 /*   By: ijaija <ijaija@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 01:23:44 by ijaija            #+#    #+#             */
-/*   Updated: 2024/05/27 18:17:30 by ijaija           ###   ########.fr       */
+/*   Updated: 2024/05/27 18:46:26 by ijaija           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ static	void	p_movemenet(t_cub *cub, int *new_x, int *new_y)
 	if (cub->p->l_r == 1)
 	{
 		step = PLAYER_SPEED;
-		(*new_x) = roundf(cub->p->mini_x_pixel + cos(cub->p->angle) * step);
-		(*new_y) = roundf(cub->p->mini_y_pixel - sin(cub->p->angle) * step);
+		(*new_x) = roundf(cub->p->mini_y_pixel - sin(cub->p->angle) * step);
+		(*new_y) = roundf(cub->p->mini_x_pixel + cos(cub->p->angle) * step);
 	}
 	else if (cub->p->l_r == -1)
 	{
 		step = PLAYER_SPEED;
-		(*new_x) = roundf(cub->p->mini_x_pixel - cos(cub->p->angle) * step);
-		(*new_y) = roundf(cub->p->mini_y_pixel + sin(cub->p->angle) * step);
+		(*new_x) = roundf(cub->p->mini_y_pixel + sin(cub->p->angle) * step);
+		(*new_y) = roundf(cub->p->mini_x_pixel - cos(cub->p->angle) * step);
 	}
 	else if (cub->p->u_d != 0)
 	{
